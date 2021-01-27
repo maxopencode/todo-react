a8a815ab67a562e13c044b9f6ad583df4a3534cb
Maksim <maxopencode@gmail.com>
2021-01-27 15:44:04 -0500

    Initial TODO app

---
+++ src/Api.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:9000/api',
+  headers: {
+    'Content-type': 'application/json'
+  }
+});
+
+export default {
+
+  getAll: () => instance.get('todos?sort=id', {
+    transformResponse: [(data) => {
+      return data ? JSON.parse(data)['_embedded'].todos : [];
+    }]
+  }),
+
+  create: (title, completed) => instance.post('todos', {title, completed}),
+
+  update: (id, title, completed) => instance.put('todos/' + id, {title, completed}),
+
+  remove: (id) => instance.delete('todos/' + id)
+}
---
+++ src/components/Todo.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import './Todo.css';
+import Api from '../Api';
+
+function Task({task, index, completeTask, removeTask}) {
+  return (
+    <div
+      className="task"
+      style={{textDecoration: task.completed ? 'line-through' : ''}}
+    >
+      {task.title}
+      <button style={{background: 'red'}} onClick={() => removeTask(index)}>x</button>
+      <button onClick={() => completeTask(index)}>Complete</button>
+    </div>
+  );
+}
+
+function CreateTask({addTask}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) {
+      return;
+    }
+
+    addTask(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add a new task"
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function Todo() {
+  const [tasks, setTasks] = useState([]);
+
+  React.useEffect(() => {
+    void Api.getAll()
+      .then(response => setTasks(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const addTask = title => {
+    void Api.create(title, false)
+      .then(response => {
+        const newTasks = [...tasks, response.data];
+        setTasks(newTasks);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const completeTask = index => {
+    const task = tasks[index];
+    void Api.update(task.id, task.title, !task.completed)
+      .then(response => {
+        const newTasks = [...tasks];
+        newTasks[index] = response.data;
+        setTasks(newTasks);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const removeTask = index => {
+    const task = tasks[index];
+    void Api.remove(task.id)
+      .then(() => {
+        const newTasks = tasks.filter(currentTask => currentTask.id !== task.id);
+        setTasks(newTasks);
+      })
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <div className="todo-container">
+      <div className="header">TODO LIST</div>
+      <div className="create-task">
+        <CreateTask addTask={addTask}/>
+      </div>
+      <div className="tasks">
+        {tasks.map((task, index) => (
+          <Task
+            task={task}
+            index={index}
+            completeTask={completeTask}
+            removeTask={removeTask}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
